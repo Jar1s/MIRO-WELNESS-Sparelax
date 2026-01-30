7d50e131b9e7167bedcee6a5e6c157d2a06d46ce
Jaroslav <jaroslav@example.com>
2026-01-30 01:04:14 +0100

    Remove Google tracking

--- wellness-families/app/layout.tsx
+++ wellness-families/app/layout.tsx
@@ -21,7 +21,6 @@ const playfair = Playfair_Display({
 });
 
 const fbPixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
-const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "GTM-T7K53SLB";
 const siteUrl =
   process.env.NEXT_PUBLIC_SITE_URL || "https://www.sparelaxbratislava.sk";
 
@@ -80,26 +79,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body className={`${inter.variable} ${playfair.variable} font-body antialiased`}>
-        {gtmId && (
-          <Script id="gtm-init" strategy="beforeInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
-            `}
-          </Script>
-        )}
-        {gtmId && (
-          <noscript
-            aria-hidden="true"
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
-        )}
-
         {fbPixelId && (
           <>
             <Script id="fb-pixel" strategy="afterInteractive">
